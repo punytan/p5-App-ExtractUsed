1b95008bfcdc633fcac9fb995fbd68bdea008540
punytan <punytan@gmail.com>
2011-12-23 04:19:31 +0900

    mock

--- lib/App/ExtractUsed.pm
+++ lib/App/ExtractUsed.pm
@@ -2,6 +2,70 @@ package App::ExtractUsed;
 use strict;
 use warnings;
 our $VERSION = '0.01';
+use Module::Extract::Use;
+use Module::CoreList;
+use File::Find;
+
+sub new {
+    my $class = shift;
+    return bless {
+        skip_namespace   => [],
+        min_perl_version => $^V,
+        @_,
+        extractor => Module::Extract::Use->new,
+    }, $class;
+}
+
+sub from_file {
+    my ($self, $file) = @_;
+    my @modules = $self->{extractor}->get_modules($file);
+    return $self->grep_non_core(@modules);
+}
+
+sub from_dir_under {
+    my ($self, $dir) = @_;
+    my %modules;
+
+    my $wanted = sub {
+        if (-f $File::Find::name) {
+            for my $module ($self->from_file($File::Find::name)) {
+                $modules{$module}++;
+            }
+        }
+    };
+
+    File::Find::finddepth({
+        wanted   => $wanted,
+        no_chdir => 1,
+    }, $dir);
+
+    return keys %modules;
+}
+
+sub grep_non_core {
+    my ($self, @used_modules) = @_;
+
+    my @requires;
+    for my $module (@used_modules) {
+        my $release = Module::CoreList->first_release($module);
+        if ($release) {
+            if ($release > $self->{min_perl_version}) {
+                # specified perl version has not the module as core
+                push @requires, $module unless $self->skip($module);
+            }
+        } else {
+            # the module is not core
+            push @requires, $module unless $self->skip($module);
+        }
+    }
+
+    return @requires;
+}
+
+sub skip {
+    my ($self, $module) = @_;
+    grep { $module =~ /^$_/ } @{ $self->{skip_namespace} };
+}
 
 1;
 __END__
